9f23233a65679232375213c8c4adc0d4530d4a65
pmgremo <pmgremo@f2a0fb1d-5e3f-4af0-b388-fe59793aa704>
2005-08-20 17:25:14 +0000

    refactored for simplicity added argument setting
    git-svn-id: svn://localhost/trunk@959 f2a0fb1d-5e3f-4af0-b388-fe59793aa704

--- src/main/java/klon/Shell.java
+++ src/main/java/klon/Shell.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.Reader;
-import java.util.LinkedList;
+import java.util.Arrays;
 
 public class Shell {
   private static final String PROMPT = "klon> ";
@@ -14,37 +14,36 @@ public class Shell {
   public void process(Reader in, PrintWriter out, PrintWriter error) {
     Compiler compiler = new Compiler();
     StringBuilder buffer = new StringBuilder();
-    LinkedList<Character> depth = new LinkedList<Character>();
-    out.print(PROMPT);
-    out.flush();
     while (true) {
+      out.print(PROMPT);
+      out.flush();
+      int depth = 0;
+      char current = (char) 0;
+      buffer.setLength(0);
       try {
-        char current = (char) in.read();
-        buffer.append(current);
-        if (OPEN_GROUP.indexOf(current) > -1) {
-          depth.addFirst(current);
+        while ("\n".indexOf(current) == -1 || depth > 0) {
+          current = (char) in.read();
+          buffer.append(current);
+          if (OPEN_GROUP.indexOf(current) > -1) {
+            depth++;
+          }
+          if (CLOSE_GROUP.indexOf(current) > -1) {
+            depth--;
+          }
         }
-        if (CLOSE_GROUP.indexOf(current) > -1) {
-          depth.poll();
-        }
-        if ("\n".indexOf(current) > -1 && depth.isEmpty()) {
-          Message message = (Message) compiler.fromString(buffer.toString());
-          KlonObject value = message.eval(Klon.ROOT, Klon.ROOT);
-          out.println(value.toString());
-          out.flush();
-          out.print(PROMPT);
-          out.flush();
-          buffer.setLength(0);
-        }
+        Message message = (Message) compiler.fromString(buffer.toString());
+        KlonObject value = message.eval(Klon.ROOT, Klon.ROOT);
+        out.println(value);
+        out.flush();
       } catch (Exception e) {
         e.printStackTrace(error);
         error.flush();
-        buffer.setLength(0);
       }
     }
   }
 
-  public static void main(String... args) {
+  public static void main(String[] args) throws Exception {
+    Klon.ROOT.setSlot("Arguments", new KlonList(Arrays.asList(args)));
     new Shell().process(new InputStreamReader(System.in), new PrintWriter(
         new OutputStreamWriter(System.out)), new PrintWriter(
         new OutputStreamWriter(System.err)));

e6fd31982334c264eb9411405bb8a3ae24758721
gremopm <gremopm@f2a0fb1d-5e3f-4af0-b388-fe59793aa704>
2005-09-14 20:02:56 +0000

    fixed bug with noop
    git-svn-id: svn://localhost/trunk@1130 f2a0fb1d-5e3f-4af0-b388-fe59793aa704

--- src/main/java/klon/KlonNoOp.java
+++ src/main/java/klon/KlonNoOp.java
@@ -7,10 +7,7 @@ public class KlonNoOp extends KlonObject {
 
   public static KlonObject newNoOp(KlonObject root, Object value)
       throws KlonObject {
-    KlonObject result = root.getSlot("NoOp")
-      .clone();
-    result.setData(value);
-    return result;
+    return root.getSlot("NoOp");
   }
 
   public KlonNoOp(State state) {
@@ -19,20 +16,13 @@ public class KlonNoOp extends KlonObject {
 
   @Override
   public KlonObject clone() {
-    KlonObject result = new KlonNoOp(state);
-    result.bind(this);
-    result.setData(data);
-    return result;
+    return this;
   }
 
   @ExposedAs("forward")
   public static KlonObject forward(KlonObject receiver, KlonObject context,
       Message message) throws KlonObject {
-    KlonObject result = (KlonObject) receiver.getData();
-    if (result == null) {
-      result = context.getSlot("Nil");
-    }
-    return result;
+    return KlonObject.noop(receiver, context, message);
   }
 
 }
